9553994f9108e86d981d0c5e443548478b458c21
Sampuccino <Sampuccino@gmail.com>
2018-06-14 00:27:59 -0700

    Some work on the textedit component.

--- AuthenticationApp/src/components/LoginForm.js
+++ AuthenticationApp/src/components/LoginForm.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
-import {View, TextInput} from 'react-native';
-import {Button, Card, CardSection} from "./common";
+import {Button, Card, CardSection, Input} from "./common";
 
 class LoginForm extends  Component{
+  state = {text: ''};
+
+
   render(){
     return(
       <Card>
         <CardSection>
-          <TextInput style={{height: 20, width: 100}} />
+          <Input
+            label="Email"
+            value={this.state.text}
+            onChangeText={text => this.setState({text:text})} />
         </CardSection>
+
         <CardSection />
+
         <CardSection>
           <Button>Log In</Button>
         </CardSection>
